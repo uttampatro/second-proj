575b589574e7ef3194a473244578b6751a9b9565
uttampatro <uttampatro890@gmail.com>
2020-12-31 00:32:48 +0530

    Deploy my react app to github pages.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//pages
+import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+//components
+import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav"
+import MovieDetail from "./pages/MovieDetail"
+//Router
+import { Switch, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
+ 
+ 
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <h1>Film</h1> */}
+       <GlobalStyle />
+       <Nav />
+       <AnimatePresence exitBeforeEnter> 
+        <Switch location={location} key={location.pathname}>
+          <Route path = "/" exact>
+              <AboutUs />
+          </Route>
+          <Route path = "/work" exact>
+            <OurWork />
+          </Route>
+          <Route path = "/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path = "/contact">
+            <ContactUs />
+          </Route>
+         </Switch>
+       </AnimatePresence>
+        
     </div>
   );
 }
